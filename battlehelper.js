82dc33a8e246aeee960ff5d570fd5a7bd32427b1
No0Priority <104585859+No0Priority@users.noreply.github.com>
2025-06-12 23:44:13 +0300

    Update battlehelper.js

--- battlehelper.js
+++ battlehelper.js
@@ -598,8 +598,8 @@ function check() {
             if (ch === 2 && lordHPs.filter(lord => {return lord !== 0}).length > 2){
                 for (const lord in lordHPs){
                     if (lordHPs[lord] === 0) continue;
-                    if (lord % 2 != 0) detailed_HPs[0] += lordHPs[lord] + "+";
-                    else detailed_HPs[1] += lordHPs[lord] + "+";
+                    if (lord % 2 != 0) detailed_HPs[1] += lordHPs[lord] + "+";
+                    else detailed_HPs[0] += lordHPs[lord] + "+";
                 }    
                 for (const i in detailed_HPs){
                     detailed_HPs[i] = `(${detailed_HPs[i].slice(0, -1)}) `;
